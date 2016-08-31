627b053817b4831e5369f35cd095ea37113ada63
Hadi Roohian <Hadi@Hadis-MacBook-Pro.local>
2016-08-31 20:56:57 +0100

    add PokeCell

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Hadi Roohian on 31/08/2016.
+//  Copyright © 2016 HadsDevs. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var pokeImage : UIImageView!
+    @IBOutlet weak var nameLbl : UILabel!
+    
+    var pokemon : Pokemon!
+    
+    func configureCell(pokemon : Pokemon) {
+        self.pokemon = pokemon
+        
+        self.pokeImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        self.nameLbl.text = self.pokemon.name
+        
+    }
+    
+    
+}
